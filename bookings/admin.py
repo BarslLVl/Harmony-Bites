2e8c31e5b9554a3c98b52e6336bb80355270e0b4
Janeks Zaremba <zarembajaneks5@gmail.com>
2024-10-24 17:31:22 +0100

    Updated admin customization for Booking and Table models

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Booking, Table
 
+# Customize the admin interface for the Booking model
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date', 'time', 'people')
+    search_fields = ('name', 'email')
+    list_filter = ('date', 'people')
+
+# Customize the admin interface for the Table model
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('number', 'capacity')
+    search_fields = ('number',)
 # Register your models here.
+admin.site.register(Booking)
+admin.site.register(Table)
